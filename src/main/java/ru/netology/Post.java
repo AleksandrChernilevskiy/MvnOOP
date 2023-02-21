52256a647e1e983dca858c0a5b3679f94979c353
Azez <aleksanderterar@gmail.com>
2023-02-21 22:56:11 +0300

    initial commit

---
+++ src/main/java/ru/netology/Post.java
@@ -0,0 +1,11 @@
+package ru.netology;
+
+public class Post {
+    public String name;
+    public String passportNumber;
+    public String patronymic;
+    public String phone;
+    public String surname;
+    public boolean subscription;
+    public FormDate birthday;
+}
